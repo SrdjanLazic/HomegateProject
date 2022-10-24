764d560aa14ab4d3ca4853feaaad61593783ec51
SrdjanLazic <slazic3219rn@raf.rs>
2022-10-24 16:44:59 +0200

    Added a new component (PropertyDetails)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header/Header";
 import SinglePropertyCard from "./components/single property card/SinglePropertyCard";
+import PropertyDetails from "./components/property details/PropertyDetails";
 
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div className="App">
         <Header/>
         <hr/>
-        <SinglePropertyCard/>
+        <div className={"layout-container"}>
+            <SinglePropertyCard/>
+            <br></br>
+            <PropertyDetails></PropertyDetails>
+        </div>
     </div>
   );
 }
--- src/components/single property card/SinglePropertyCard.js
+++ src/components/single property card/SinglePropertyCard.js
@@ -3,40 +3,46 @@ import './SinglePropertyCard.css';
 
 function SinglePropertyCard(props) {
     return (
-        <div class="card">
-            <div class="images">
-                <div class="main-image">
-                    <img
-                        src="https://media2.homegate.ch/f_auto/t_web_dp_large/listings/hgonif/3002141007/image/42d0289f48469599ab639d066bb3d9b4.jpg"/>
+        <div class={"container"}>
+            <div class="card">
+                <div className={"images"}>
+                    <div class="main-image">
+                        <img
+                            src="https://media2.homegate.ch/f_auto/t_web_dp_large/listings/hgonif/3002141007/image/42d0289f48469599ab639d066bb3d9b4.jpg"/>
+                    </div>
+                    <div class="other-images">
+                        <img
+                            src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/56a1f6ef9af4ef235de2cce09afc9ab3.jpg"/>
+                        <img
+                            src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/22e6d9d9f917a7acaa7c743a347613ce.jpg"/>
+                        <img
+                            src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/882580fa7da510c9bbd90c7d30abfb87.jpg"/>
+                    </div>
                 </div>
-                <div class="other-images">
-                    <img src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/56a1f6ef9af4ef235de2cce09afc9ab3.jpg"/>
-                    <img src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/22e6d9d9f917a7acaa7c743a347613ce.jpg"/>
-                    <img src="https://media2.homegate.ch/f_auto/t_web_320/listings/hgonif/3002141007/image/882580fa7da510c9bbd90c7d30abfb87.jpg"/>
-                </div>
-            </div>
-            <div class="info">
-                <div class="title">
-                    <h4>CHF3,500,000.–</h4>
-                    <h4>150m2</h4>
-                    <h4>5.5rm</h4>
-                    <p>Premium</p>
-                </div>
-                <hr/>
-                <div class="description">
-                    <p>8047 Zürich</p>
-                    <p>***********Details zur Liegenschaft (inkl. Dokumentation) finden Sie in der beigelegten
-                        Broschüre.********An der ruhigen Spiserstrasse im Zürcher Trendquartier Albisrieden (Stadtkreis
-                        9) unweit vom
-                        Stadtzentrum befindet sich das 1929 erbaute Doppeleinfamilienhaus auf einem sonnigen, 387m2
-                        grossen Grundstück. Perfekt gelegen zur Stadt und zum Naherholungsgebiet bietet es den
-                        idealen Ausgleich zwischen Stadtleben und Natur.Schon der 239 m2 grosse Vorgarten mit seiner
-                        sonnigen Terrasse lädt die Liegenschafts-Besucher zum</p>
-                    <button>Contact the advertiser</button>
-                </div>
-                <hr/>
-                <div class="footer">
-                    <p>Save</p>
+                <div class="info">
+                    <div class="title">
+                        <h4>CHF3,500,000.–</h4>
+                        <h4>150m2</h4>
+                        <h4>5.5rm</h4>
+                        <p>Premium</p>
+                    </div>
+                    <hr/>
+                    <div class="description">
+                        <p>8047 Zürich</p>
+                        <p>***********Details zur Liegenschaft (inkl. Dokumentation) finden Sie in der beigelegten
+                            Broschüre.********An der ruhigen Spiserstrasse im Zürcher Trendquartier Albisrieden
+                            (Stadtkreis
+                            9) unweit vom
+                            Stadtzentrum befindet sich das 1929 erbaute Doppeleinfamilienhaus auf einem sonnigen, 387m2
+                            grossen Grundstück. Perfekt gelegen zur Stadt und zum Naherholungsgebiet bietet es den
+                            idealen Ausgleich zwischen Stadtleben und Natur.Schon der 239 m2 grosse Vorgarten mit seiner
+                            sonnigen Terrasse lädt die Liegenschafts-Besucher zum</p>
+                        <button>Contact the advertiser</button>
+                    </div>
+                    <hr/>
+                    <div class="footer">
+                        <p>Save</p>
+                    </div>
                 </div>
             </div>
         </div>
