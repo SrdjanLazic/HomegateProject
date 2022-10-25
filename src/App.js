2282b387f0d6f35e84805444acf83c3ae3a94a97
SrdjanLazic <slazic3219rn@raf.rs>
2022-10-25 15:03:54 +0200

    Added home, search and details routes; some visual changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import './App.css';
 import Header from "./components/header/Header";
 import SinglePropertyCard from "./components/single property card/SinglePropertyCard";
 import PropertyDetails from "./components/property details/PropertyDetails";
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/home/Home";
 
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <hr/>
-        <div className={"layout-container"}>
-            <SinglePropertyCard/>
-            <br></br>
-            <PropertyDetails></PropertyDetails>
-        </div>
-    </div>
+    // <div className="App">
+    //     <Header/>
+    //     <hr/>
+    //     <div className={"layout-container"}>
+    //         <SinglePropertyCard/>
+    //         <br></br>
+    //         <PropertyDetails></PropertyDetails>
+    //     </div>
+    // </div>
+      <Routes>
+          <Route path={"/"} element={ <Home/> }/>
+          <Route path={"/search"} element={ <SinglePropertyCard/> }/>
+          <Route path={"/details"} element={ <PropertyDetails/> }/>
+      </Routes>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,46 +1,49 @@
 import React from 'react';
 import './Header.css';
+import { Link } from "react-router-dom";
 
 function Header(){
     return (
-        <navbar>
-            <ul className={"nav-list"}>
-                <div className={"nav-list-left"}>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Homegate</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Submit ad</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Rent</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Buy</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Mortgages</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Moving</a>
-                    </li>
-                    <li className={"nav-item"}>
-                        <a href={"#"}>Advisor</a>
-                    </li>
-                </div>
-                <div className={"nav-list-right"}>
-                    <li className="nav-item">
-                        <a href={"#"}>Favourites</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href={"#"}>User account</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href={"#"}>EN</a>
-                    </li>
-                </div>
-            </ul>
-        </navbar>
+        <header>
+            <nav>
+                <ul className={"nav-list"}>
+                    <div className={"nav-list-left"}>
+                        <li className={"nav-item"}>
+                            <Link to={"/"}>Homegate</Link>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Submit ad</a>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Rent</a>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Buy</a>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Mortgages</a>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Moving</a>
+                        </li>
+                        <li className={"nav-item"}>
+                            <a href={"#"}>Advisor</a>
+                        </li>
+                    </div>
+                    <div className={"nav-list-right"}>
+                        <li className="nav-item">
+                            <a href={"#"}>Favourites</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href={"#"}>User account</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href={"#"}>EN</a>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
